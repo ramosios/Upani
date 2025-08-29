9a024321374adb2c3277df21a96457f33abf8e69
jorgeramos2 <jorgeramos.1996@outlook.com>
2025-08-29 23:10:57 +0200

    Remove json anime

--- AniCue/Resources/DownloadedAnimesFileNames.swift
+++ AniCue/Resources/DownloadedAnimesFileNames.swift
@@ -7,10 +7,9 @@
 import Foundation
 
 struct DownloadedAnimesFileNames {
-    static let initialFileNames = ["top_1-400_anime", "top_401-800_anime", "top_801-1200_anime"]
     static let fileNames: [String] = {
         var names: [String] = []
-        for i in stride(from: 1201, to: 12001, by: 400) {
+        for i in stride(from: 1, to: 12001, by: 400) {
             let start = i
             let end = i + 399
             names.append("top_\(start)-\(end)_anime")
