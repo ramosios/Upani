5e049c57a7563456aff69eed4530c5b72d97f671
jorgeramos2 <jorgeramos.1996@outlook.com>
2025-10-31 10:04:59 +0100

    Added main menu logic
    Make MatchMenu view reusable renamed to menuview

---
+++ AniCue/Views/Games/Match/MenuView.swift
@@ -0,0 +1,58 @@
+import SwiftUI
+
+struct MenuView: View {
+    enum MenuSourceType {
+        case mainMenu
+        case match
+
+        var title: String {
+            switch self {
+            case .match:
+                return "Select Category"
+            case .mainMenu:
+                return "Select Game"
+            }
+        }
+
+        var menuItems: [MenuItem] {
+            switch self {
+            case .match:
+                return Constants.matchingMenuItem
+            case .mainMenu:
+                return Constants.menuItem
+            }
+        }
+        @ViewBuilder
+        func destination(for item: MenuItem) -> some View {
+            switch self {
+            case .match:
+                MatchView(source: item.name)
+            case .mainMenu:
+                MenuView(sourceType: .match)
+            }
+        }
+    }
+
+    let sourceType: MenuSourceType
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 24) {
+                    ForEach(sourceType.menuItems) { item in
+                        NavigationLink(destination: sourceType.destination(for: item)) {
+                            MenuCardView(menuItem: item)
+                        }
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle(sourceType.title)
+        }
+        .accentColor(.teal)
+    }
+}
--- AniCue/Views/Tab/TabBarView.swift
+++ AniCue/Views/Tab/TabBarView.swift
@@ -23,7 +23,7 @@ struct TabBarView: View {
                 .tabItem {
                     Label(CustomTabBarView.Tab.watchlist.title, systemImage: CustomTabBarView.Tab.watchlist.iconName)
                 }
-            MatchMenuView()
+            MenuView(sourceType: .mainMenu)
                 .tag(CustomTabBarView.Tab.games)
                 .tabItem {
                     Label(CustomTabBarView.Tab.games.title, systemImage: CustomTabBarView.Tab.games.iconName)
