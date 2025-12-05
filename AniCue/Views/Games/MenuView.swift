f12414b8749ee5e8d7a21114eeca63b61786e268
jorgeramos2 <jorgeramos.1996@outlook.com>
2025-12-05 21:55:08 +0100

    Refactor MenuSourceType to Models layer
    Moved the MenuSourceType enum and its logic from MenuView.swift to MenuItem.swift in the Models layer for better separation of concerns and code organization.

--- AniCue/Views/Games/MenuView.swift
+++ AniCue/Views/Games/MenuView.swift
@@ -1,47 +1,6 @@
 import SwiftUI
 
 struct MenuView: View {
-    enum MenuSourceType {
-        case mainMenu
-        case match
-
-        var title: String {
-            switch self {
-            case .match:
-                return "Select Category"
-            case .mainMenu:
-                return "Select Game"
-            }
-        }
-
-        var menuItems: [MenuItem] {
-            switch self {
-            case .match:
-                return Constants.matchingMenuItem
-            case .mainMenu:
-                return Constants.menuItem
-            }
-        }
-        @ViewBuilder
-        func destination(for item: MenuItem) -> some View {
-            switch self {
-            case .match:
-                MatchView(source: item.name)
-            case .mainMenu:
-                switch item.name {
-                case "Matching":
-                    MenuView(sourceType: .match)
-                case "Search":
-                    SearchView()
-                case "New Anime":
-                    NewAnimeView()
-                default:
-                    MenuView(sourceType: .mainMenu)
-                }
-            }
-        }
-    }
-
     let sourceType: MenuSourceType
     let columns = [
         GridItem(.flexible()),
